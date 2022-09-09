938ccc3784b5b730a52cd999ff4aba0d72f809b0
anoy369 <adannoy369@gmail.com>
2022-09-09 10:01:37 +0600

    basic architecture

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import About from './components/about/about'
+import Contact from './components/contact/contact'
+import Experience from './components/experience/experience'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import Portfolio from './components/portfolio/portfolio'
+import Services from './components/services/services'
+import Testimonials from './components/testimonials/testimonials'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
