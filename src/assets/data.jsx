514976a30d7dc57eb4630e5055d47f3547cc324a
anoy369 <adannoy369@gmail.com>
2022-09-10 07:03:49 +0600

    designed portfolio & get portfolio item from array

---
+++ src/assets/data.jsx
@@ -0,0 +1,40 @@
+import Image from './image.jpg'
+const data=[
+    {
+        id: 1,
+        image: Image,
+        title: 'Item Title',
+        github: '#',
+        demo: '#'
+    },
+    {
+        id: 2,
+        image: Image,
+        title: 'Item Title',
+        github: '#',
+        demo: '#'
+    },
+    {
+        id: 3,
+        image: Image,
+        title: 'Item Title',
+        github: '#',
+        demo: '#'
+    },
+    {
+        id: 4,
+        image: Image,
+        title: 'Item Title',
+        github: '#',
+        demo: '#'
+    },
+    {
+        id: 5,
+        image: Image,
+        title: 'Item Title',
+        github: '#',
+        demo: '#'
+    }
+]
+
+export default data;
