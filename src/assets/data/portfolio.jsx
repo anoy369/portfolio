83ebbb1ea6b3b9cbf884fe44763fced9fa7a7108
anoy369 <adanoy369@gmail.com>
2023-03-14 12:33:50 +0600

    update projects

--- src/assets/data/portfolio.jsx
+++ src/assets/data/portfolio.jsx
@@ -1,12 +1,12 @@
 import Image from '../image.jpg'
 import doctor from '../doctors directory.png'
 const portfolio=[
-    {
-        id: 1,
-        image: doctor,
-        title: 'Doctors Directory with wordpress',
-        demo: 'https://doctors.anoy369.com/'
-    },
+    // {
+    //     id: 1,
+    //     image: doctor,
+    //     title: 'Doctors Directory with wordpress',
+    //     demo: 'https://doctors.anoy369.com/'
+    // },
     {
         id: 2,
         image: Image,
