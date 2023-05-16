9acdc221596845efe07acadc6b15964550cb1b29
anoy369 <adanoy369@gmail.com>
2023-05-17 03:07:42 +0600

    updated projects

--- src/assets/data/portfolio.jsx
+++ src/assets/data/portfolio.jsx
@@ -1,18 +1,35 @@
 import Image from '../image.jpg'
 import doctor from '../doctors directory.png'
+import netflix from '../netflixclone.png'
+import flagship from '../flagship.jpg'
+import chess from '../chess.jpg'
 const portfolio=[
-    // {
-    //     id: 1,
-    //     image: doctor,
-    //     title: 'Doctors Directory with wordpress',
-    //     demo: 'https://doctors.anoy369.com/'
-    // },
+    {
+        id: 1,
+        image: doctor,
+        title: 'Doctors Directory',
+        demo: 'https://genesisobgyn.net/'
+    },
     {
         id: 2,
-        image: Image,
+        image: netflix,
         title: 'Netflix with react',
         github: 'https://github.com/anoy369/react-netflix',
     },
+    {
+        id: 3,
+        image: flagship,
+        title: 'e-commerce',
+        demo: 'https://flagshipstorebd.com/'
+
+    },
+    {
+        id: 4,
+        image: chess,
+        title: 'Multiplayer chess with video calling',
+        github: 'https://github.com/anoy369/multimedia-chess'
+
+    }
 ]
 
 export default portfolio;
